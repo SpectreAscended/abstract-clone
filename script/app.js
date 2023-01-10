96fd1cad80366513bfd02682a082bc473a11e666
Cory Pollard <astandard87@gmail.com>
2023-01-09 20:53:45 -0600

    Added mobile help-center

--- script/app.js
+++ script/app.js
@@ -1,5 +1,7 @@
 const mobileMenu = document.querySelector('.mobile-menu');
+const mobileHelp = document.querySelector('.mobile-help-center');
 
 mobileMenu.addEventListener('click', function (e) {
   mobileMenu.classList.toggle('active');
+  mobileHelp.classList.toggle('hidden');
 });
