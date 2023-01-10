274f72577fdcd058fe0bec495c9227cc17697606
Cory Pollard <astandard87@gmail.com>
2023-01-09 21:42:18 -0600

    fixed problem with mobile-help-center. made position absolute

--- script/app.js
+++ script/app.js
@@ -1,7 +1,7 @@
 const mobileMenu = document.querySelector('.mobile-menu');
 const mobileHelp = document.querySelector('.mobile-help-center');
 
-mobileMenu.addEventListener('click', function (e) {
+mobileMenu.addEventListener('click', () => {
   mobileMenu.classList.toggle('active');
   mobileHelp.classList.toggle('hidden');
 });
